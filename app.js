2ffa3401ed82d97394ca11efac99c98ea09db8b8
Marcelo <marcelo.muniz@platformbuilders.io>
2019-09-20 14:39:27 -0300

    inicio de projeto

---
+++ app.js
@@ -0,0 +1,14 @@
+new Vue(
+    {
+        el: '#app',
+        data: {
+            chemicals : [],
+            isNew: false,
+            selectedBrand : 0,
+            selectedFood : 0,
+        },
+        methods: {
+
+        }
+    }
+)
