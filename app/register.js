85304c0c71f650209b6f85940e69b12c70328015
Marcelo <marcelo.muniz@platformbuilders.io>
2019-11-28 07:42:07 -0300

    corrigindo servidor

--- app/register.js
+++ app/register.js
@@ -1,6 +1,6 @@
-server = 'http://localhost:5000';
+//server = 'http://localhost:5000';
 
-//server = 'http://35.229.29.128:5000';
+server = 'http://35.229.29.128:5000';
 new Vue(
     {
         el: '#app',
