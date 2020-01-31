c04c64931ba12b24c518c28f92a425e4b1aa280b
mmuniz75 <mmunizs1975@gmail.com>
2020-01-31 09:28:05 -0300

    link google par aos produtos

--- src/components/Chemical.vue
+++ src/components/Chemical.vue
@@ -1,9 +1,12 @@
 <template>
     <div v-if="items.length > 0" >
-           <h5 class="card-title mb-4 mt-4">Produtos Quimicos desse produto</h5>
-            <ul class="list-group list-group-flush mb-3" v-for="(chemical, index) in items" :key="index">
+           <h5 class="card-title mt-4">Produtos Quimicos desse produto :</h5>
+           <span v-if="items.length > 0 && items[0] !== 'Nenhum'" class="card-title mt-4 mb-2" ><i>Click no produto para saber mais</i></span>
+            <ul class="list-group list-group-flush mb-3 mt-4" v-for="(chemical, index) in items" :key="index">
                 <li class="list-group-item alert-success" >
+                    <a :href="googleUrl+chemical" target="_blank">
                     <span :class="chemical=='Nenhum'?'alert-success font-weight-bold':''">{{chemical}}</span>
+                    </a>
                 </li>
             </ul>
     </div>
@@ -13,6 +16,11 @@
   export default {
     props: {
       items: Array
+    },
+    data : function(){
+      return {
+        googleUrl : process.env.VUE_APP_GOOGLE_URL_QUERY
+      }
     }
   }  
 </script>
