d44890090957b7091d482e49e0ece08e02b90961
mmuniz75 <mmunizs1975@gmail.com>
2020-01-31 10:00:39 -0300

    link so para quem tem quimicos

--- src/components/Chemical.vue
+++ src/components/Chemical.vue
@@ -1,10 +1,10 @@
 <template>
     <div v-if="items.length > 0" >
            <h5 class="card-title mt-4">Produtos Quimicos desse produto :</h5>
-           <span v-if="items.length > 0 && items[0] !== 'Nenhum'" class="card-title mt-4 mb-2" ><i>Click no produto para saber mais</i></span>
+           <span v-if="hasChemical()" class="card-title mt-4 mb-2" ><i>Click no produto para saber mais</i></span>
             <ul class="list-group list-group-flush mb-3 mt-4" v-for="(chemical, index) in items" :key="index">
                 <li class="list-group-item alert-success" >
-                    <a :href="googleUrl+chemical" target="_blank">
+                    <a href="#" @click="getHref(chemical)">
                     <span :class="chemical=='Nenhum'?'alert-success font-weight-bold':''">{{chemical}}</span>
                     </a>
                 </li>
@@ -17,9 +17,16 @@
     props: {
       items: Array
     },
-    data : function(){
-      return {
-        googleUrl : process.env.VUE_APP_GOOGLE_URL_QUERY
+    methods : {
+      getHref(chemical) {
+        if (this.hasChemical()) {
+          window.open(process.env.VUE_APP_GOOGLE_URL_QUERY + chemical)
+        }else {
+          return false  
+        }  
+      },
+      hasChemical(){
+        return this.items.length > 0 && this.items[0] !== 'Nenhum'
       }
     }
   }  
